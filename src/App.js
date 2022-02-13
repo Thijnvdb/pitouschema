cae2d78ba260d27a4919880f2cb8ee93f2cb9ef9
Thijnvdb <thijnvandenbergh@gmail.com>
2022-02-13 21:02:29 +0100

    tried fixing github pages

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import './wordart.scss';
+
+Date.prototype.getWeekNumber = function(){
+  var d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
+  var dayNum = d.getUTCDay() || 7;
+  d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+  var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+  return Math.ceil((((d - yearStart) / 86400000) + 1)/7)
+};
+
+function App() {
+  const people = ["Bram", "Isa", "Thijn", "Thomas", "Frederik"]
+  const styles = ["rainbow", "blues", "superhero", "radial", "tilt", "purple", "horizon", "slate"]
+  const [person, setPerson] = useState();
+  const [wordart, setWordart] = useState();
+
+  useEffect(()=> {
+    let week = new Date().getWeekNumber();
+    setPerson(people[week % people.length]);
+    setWordart(styles[Math.floor(Math.random() * styles.length)]);
+  },[]);
+
+  function getRandomAnimationDirection() {
+    return Math.floor(Math.random() * 2) === 1 ? "reverse" : ""
+  }
+
+  const fotocount = 3;//dit is het aantal foto's in de array
+  function randomArray() {
+    const pietcount = 5;//dit gelijk maken aan pitou foto aantal
+
+    let arr = [].fill(0,0,fotocount);
+    for(let i = 0; i < fotocount; i++) {
+      arr[i] = Math.floor(Math.random() * pietcount) + 1;
+    }
+
+    return arr;
+  }
+
+  return (
+      <div style={{backgroundImage: `url(${process.env.PUBLIC_URL}/tile.gif)`}} className="App bg-cover flex justify-evenly flex-col items-center">
+        <div className={`pitouholder`}>
+          {
+            randomArray().map((x,i) => <img key={i+"a"} src={`${process.env.PUBLIC_URL}/pitou${x}.png`} className={"z-10 max-w-1/3  pitou "+ getRandomAnimationDirection()}/>)
+          }
+        </div>
+        <h2 className='text-5xl z-10 text-center'>Deze week mag...</h2>
+        <h1 className={'z-10 wordart text-7xl lg:text-9xl ' + wordart}><span className='text'>{person}</span></h1>
+        <h2 className='text-5xl z-10 text-center'>De bak verschonen!</h2>
+        <div className={`pitouholder`}>
+          {
+            randomArray().map((x,i) => <img key={i+"b"} src={`${process.env.PUBLIC_URL}/pitou${x}.png`} className={"z-10 max-w-1/3 pitou "+ getRandomAnimationDirection()}/>)
+          }
+        </div>
+      </div>
+  );
+}
+
+export default App;
